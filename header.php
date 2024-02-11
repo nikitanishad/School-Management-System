d6d6eb17e55d271c11ee52a7cc67408269ebaf2d
nikita nishad <nikitanishad001@gmail.com>
2024-02-11 11:01:12 +0530

    Add some code

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SMS</title> 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="css/style.css">
+ </head>
+<body>
+<section>
+ <div class="container">
+     <!-- navbar -->
+    <nav class="navbar">
+      <a href="#" class="sms-item"><b>SMS</b></a>
+      <div class="list">
+        <div class="list-item1">
+         <ul>
+           <li><a href="#">Home</a></li>
+           <li><a href="#">About us</a></li>
+           <li><a href="#">Courses</a></li>
+           <li><a href="#">Events</a></li>
+           <li><a href="#">Home</a></li>
+         </ul>
+        </div>
+        <div class="list-item2">
+          <ul>
+            <li><i class="fa-solid fa-user"></i><a href="">Users login</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav> 
