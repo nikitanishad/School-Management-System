49631de61d71868ca747916463e79f9bfe509611
nikita nishad <nikitanishad001@gmail.com>
2024-02-14 23:32:22 +0530

    add code

--- admin/index.php
+++ admin/index.php
@@ -8,93 +8,95 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>  
-  <!-- navbar sections -->
+  <!-- sidebar  -->
+  <div class="sidebar">
+
+    <div class="left-sidebar">
+      <div class="student-panel">
+        <a href="#">
+         <img src="" alt="">
+         <span>Student Panel</span>
+       </a>
+     </div>
+     <div class="left-dashboard">
+       <ul>
+         <li class="list-icons">
+           <a href="#"><i class="nav-icon fa-solid fa-gauge"></i>Dashboard</a>
+          </li>
+          <li>
+           <a href="#"><i class="nav-icon fas fa-chalkboard"></i><p>Manage Syllabus</p><i class="fas fa-angle-left right"></i></a>
+          </li>
+         <li>
+           <a href="#"><i class="nav-icon fas fa-chalkboard-teacher"></i><p>Manage Class Routines</p> <i class="fas fa-angle-left right"></i></a>
+         </li>
+          <li>
+            <a href="#"><i class="nav-icon fas fa-file-alt"></i><p>Manage Examinations</p><i class="fas fa-angle-left right"></i> </a>
+          </li>
+         <li>
+           <a href="#"><i class="nav-icon fas fa-calendar-alt"></i><p>Manage Attendance</p><i class="fas fa-angle-left right"></i></a>
+         </li>
+          <li>
+            <a href="#"><i class="nav-icon fas fa-money-check"></i><p>Fee Details </p>
+            <i class="fas fa-angle-left right"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="nav-icon fas fa-paste"></i><p>Study Materials</p>
+            <i class="fas fa-angle-left right"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="nav-icon fas fa-calendar-check"></i><p>Manage Events</p><i class="fas fa-angle-left right"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="nav-icon fas fa-users"></i><p>Communications</p>
+            <i class="fas fa-angle-left right"></i></a>
+          </li>
+       </ul>
+       <div></div>
+       <div></div>
+      </div>
+    </div>
+
+ <div class="right-sidebar">
+   <!-- navbar sections -->
+   <aside>
+      <nav class="admin-nav">
+       <ul class="nav-link1">
+        <li><a href="#"><i class="fa-solid fa-bars"></i></a></li>
+        <li><a href="#">Profile</a></li>
+       
+       </ul>
+       <ul class="nav-link2">
+        <li><a href="#">
+         <i class="fa-solid fa-comment"></i>
+         </a>
+        </li>
+        <li><a href="#">
+         <i class="fa-solid fa-bell"></i>
+        </a></li>
+        <li><a href="#">Logout<i class="fa fa-sign-out-alt"></i></a></li>
+       </ul>
+      </nav>
+   </aside>
+
+    <!-- content sections -->
+
+
 <aside>
-  <nav class="admin-nav">
-   <ul class="nav-link1">
-    <li><a href="#"><i class="fa-solid fa-bars"></i></a></li>
-    <li><a href="#">Profile</a></li>
-   
-   </ul>
-   <ul class="nav-link2">
-    <li><a href="#">
-     <i class="fa-solid fa-comment"></i>
-     </a>
-    </li>
-    <li><a href="#">
-     <i class="fa-solid fa-bell"></i>
-    </a></li>
-    <li><a href="#">Logout<i class="fa fa-sign-out-alt"></i></a></li>
-   </ul>
-  </nav>
-</aside>
-
-<!-- sidebar  -->
-<section>
-<div class="left-sidebar">
-  <div class="student-panel">
-    <a href="#">
-      <img src="" alt="">
-     <span>Student Panel</span>
-    </a>
-  </div>
-   <div class="left-dashboard">
-     <ul>
-      <li>
-        <a href="#"><i class="fa-solid fa-gauge"></i><p>Dashboard</p></a>
-     </li>
-      <li>
-        <a href="#"><i class="nav-icon fas fa-chalkboard"></i><p>Manage Syllabus</p><i class="fas fa-angle-left right"></i></a>
-     </li>
-      <li>
-        <a href="#"><i class="nav-icon fas fa-chalkboard-teacher"></i><p>Manage Class Routines<i class="fas fa-angle-left right"></i></p></a>
-      </li>
-      <li>
-        <a href="#"><i class="nav-icon fas fa-file-alt"></i><p>Manage Examinations</p><i class="fas fa-angle-left right"></i> </a>
-      </li>
-      <li>
-        <a href="#"><i class="nav-icon fas fa-calendar-alt"></i><p>Manage Attendance</p><i class="fas fa-angle-left right"></i></a>
-      </li>
-      <li>
-        <a href="#"><i class="nav-icon fas fa-money-check"></i><p>Fee Details </p>
-        <i class="fas fa-angle-left right"></i></a>
-      </li>
-      <li>
-        <a href="#"><i class="nav-icon fas fa-paste"></i><p>Study Materials </p>
-        <i class="fas fa-angle-left right"></i></a>
-      </li>
-      <li>
-        <a href="#"><i class="nav-icon fas fa-calendar-check"></i><p>Manage Events</p><i class="fas fa-angle-left right"></i></a>
-      </li>
-      <li>
-        <a href="#"><i class="nav-icon fas fa-users"></i><p>Communications</p><i class="fas fa-angle-left right"></i></a>
-      </li>
-     </ul>
-     <div></div>
-     <div></div>
-   </div>
-</div>
-</section>
-
-<section>
-  <div class="content-header">
+ <div class="content">
+ <div class="content-header">
     <div class="header-txt1">
       <h1>Dashboard</h1>
     </div>
     <div class="header-txt2">
       <ol>
         <li class=""><a href="#">Student</a></li>
-        <li><a href="#">Dashboard</a></li> 
+        <li>Dashboard</li> 
       </ol>
     </div>
   </div>
-</section>
-
-<aside>
- <div class="content">
    <div class="content-row">
        <!-- student -->
-     <div class="student">
+     <div class="student content-info">
         <div class="cap"><span><i class="fas fa-graduation-cap"></i></span></div>
         <div class="student-info">
          <span class="info-box">Total Students</span>
@@ -104,8 +106,8 @@
       <div>
 
          <!-- Teachers -->
-       <div class="teachers">
-         <div class="teacher"><span><i class="fas fa-users"></i></span></div>
+       <div class="teachers content-info">
+         <div class="teacher-icons"><span><i class="fas fa-users"></i></span></div>
          <div class="teacher-info">
            <span class="info-box">Total Teachers</span>
            <span class="info-box-number">50+</span>
@@ -114,7 +116,7 @@
       </div>
       <div>
           <!-- course -->
-      <div class="course">
+      <div class="course content-info">
         <div class="course-icon"><span><i class="fas fa-book-open"></i></span></div>
         <div class="course-info">
          <span class="info-box">Total Courses</span>
@@ -125,35 +127,27 @@
       <div>
 
         <!-- inquiries -->
-      <div class="in-question">
-        <div class="questions"><span><i class="fas fa-question"></i></span></div>
+      <div class="in-question content-info">
+        <div class="questions-icon"><span><i class="fas fa-question"></i></span></div>
         <div class="inquiries-info">
          <span class="info-box">Total Students</span>
-         <span class="info-box">10</span>
+         <span class="info-box-number">10</span>
         </div>
       </div>
       </div>
    </div>
-   <hr>
-   <br>
-   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. At quis quia dolorum labore! Magnam sunt, voluptates soluta, veniam consectetur quo vitae, tenetur nemo fuga non dolor ipsam beatae quisquam nesciunt!
-
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia alias, porro beatae incidunt rem necessitatibus voluptate! Minima corporis, necessitatibus quasi autem mollitia maxime provident tempore perferendis dolorem expedita amet ullam.
-
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt doloremque voluptatibus iure veritatis in quas tenetur cum, architecto nulla repellat laudantium, deleniti nam temporibus facere, reiciendis quam dignissimos ipsam! Commodi!
-   </p>
   </div>
 </aside>
 
-<!-- footer sections -->
+   <!-- footer sections -->
 
-<div class="admin-footer">
-  <strong>
-    Copyright © 2024 <a href="#">School Management System</a>
-    All rights reserved.
+    <div class="admin-footer">
+     <strong>
+         Copyright © 2024 <a href="#">School Management System</a>
+         All rights reserved.
 
-  </strong>
-</div>
+      </strong>
+    </div>
+  </div>
 </body>
 </html>
